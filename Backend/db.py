ec166a3724830c44e197f67b17cf2515d40dbd72
unknown <deepaksureshts@gmail.com>
2023-03-20 21:00:59 +0530

    docker Backend db

--- Backend/db.py
+++ Backend/db.py
@@ -1,10 +1,15 @@
 import pymongo 
+from dotenv import dotenv_values
+import urllib.parse
 
-conn = pymongo.MongoClient('localhost', 27017)
-db = conn[ "SCMDb" ]
+username = urllib.parse.quote_plus('deepak')
+password = urllib.parse.quote_plus("demo@123")
+
+url = "mongodb+srv://{}:{}@scmxpert.73z8rrw.mongodb.net/?retryWrites=true&w=majority".format(username, password)
+
+client = pymongo.MongoClient(url)
+
+db =client["SCMXpert"]
 collection_name = db["newuser"]
 collection_shipment = db["shipmentdetails"]
 collection_device=db["device_data"]
-
-# bootstrap_servers = ['localhost:9092']
-# topicName = 'device_data'
