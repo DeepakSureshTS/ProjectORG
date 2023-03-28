c92f7799b7a008f69a682fc3f41518fdaa6e6be1
unknown <deepaksureshts@gmail.com>
2023-03-28 21:45:21 +0530

    local code set

--- Frontend/static/js/createShipment.js
+++ Frontend/static/js/createShipment.js
@@ -64,5 +64,6 @@ async function createShipment(){
 }
 function logout(){
     localStorage.removeItem('access_token')
-    window.location.href = "../../templates/Login.html"
+    // window.location.href = "../../templates/Login.html"
+    window.location.href = "http://"+window.location.hostname+":5500/../../Frontend/templates/Login.html";
   }
