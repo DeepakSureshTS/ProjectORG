3f1436db0e75394dd08ff94ba48cf76b50cdc8fb
unknown <deepaksureshts@gmail.com>
2023-04-03 21:15:40 +0530

    demo changes

--- Frontend/static/js/deviceData.js
+++ Frontend/static/js/deviceData.js
@@ -1,5 +1,5 @@
   function getDeviceData(x) {
-    x.style.display = 'none';
+ 
     var token = localStorage.getItem("access_token");
                 if(token == undefined){                  
                     //  window.location.href = "http://"+window.location.hostname+":5501"
@@ -16,14 +16,16 @@
         console.log(data);
         if (data.length > 0) {
           var temp = "";
+          
           data.forEach((itemData) => {
-            temp += "<tr>";
-            temp += "<td>" + itemData.Battery_Level + "</td>";
-            temp += "<td>" + itemData.Device_Id + "</td>";
-            temp += "<td>" + itemData.First_Sensor_temperature + "</td>";
-            temp += "<td>" + itemData.Route_From + "</td>";
-            temp += "<td>" + itemData.Route_To + "</td></tr>";
-          });
+            if (itemData.Device_Id ==  document.getElementById('deviceId').value){
+              temp += "<tr>";
+              temp += "<td>" + itemData.Battery_Level + "</td>";
+              temp += "<td>" + itemData.Device_Id + "</td>";
+              temp += "<td>" + itemData.First_Sensor_temperature + "</td>";
+              temp += "<td>" + itemData.Route_From + "</td>";
+              temp += "<td>" + itemData.Route_To + "</td></tr>";
+         }});
           document.getElementById('data').innerHTML = temp;
         }
       }
